be83d2a869c233e56ec2da2adcda83147e4dc28b
Lo Leon <lonelyliar8523@gmail.com>
2023-10-11 16:52:24 +0800

    rust 121

--- rust/121.best-time-to-buy-and-sell-stock.rs
+++ rust/121.best-time-to-buy-and-sell-stock.rs
@@ -9,8 +9,8 @@ impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let (mut buy, mut profit) = (prices[0], 0);
         for i in 1..prices.len() {
-            buy = i32::min(buy, prices[i]);
-            profit = i32::max(profit, prices[i] - buy);
+            buy = buy.min(prices[i]);
+            profit = profit.max(prices[i] - buy);
         }
         return profit;
     }
