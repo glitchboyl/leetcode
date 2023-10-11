9999a0bcb425db83e64410e847efb4c54d301906
Lo Leon <lonelyliar8523@gmail.com>
2023-10-11 15:05:21 +0800

    rust 367

--- rust/367.valid-perfect-square.rs
+++ rust/367.valid-perfect-square.rs
@@ -7,12 +7,7 @@
 // @lc code=start
 impl Solution {
     pub fn is_perfect_square(num: i32) -> bool {
-        // consraint x <= 2^31 - 1
-        let sqrt_limit = 46340;
-        let (mut left, mut right) = (1, num / 2);
-        if right > sqrt_limit {
-            right = sqrt_limit;
-        }
+        let (mut left, mut right) = (1, i32::min(x / 2, 46340));
         while left < right {
             let mid = left + (right - left) / 2;
             if mid * mid == num {
