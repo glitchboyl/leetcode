aa4bf6d028c7db8abf5eed4170501fbc28548546
Lo Leon <lonelyliar8523@gmail.com>
2023-10-12 16:31:27 +0800

    rust 27: added solution of two pointers

--- rust/27.remove-element.rs
+++ rust/27.remove-element.rs
@@ -7,9 +7,20 @@
 // @lc code=start
 impl Solution {
     pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-        nums.retain(|&num| num != val);
-        return nums.len() as i32;
+        // Solution 1:
+        // nums.retain(|&num| num != val);
+        // return nums.len() as i32;
+
+        // Solution 2: two pointers
+        let (mut s, mut f) = (0, 0);
+        while f < nums.len() {
+            if nums[f] != val {
+                nums.swap(s, f);
+                s += 1;
+            }
+            f += 1;
+        }
+        return s as i32;
     }
 }
 // @lc code=end
-
