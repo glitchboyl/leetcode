4a7c7ace0ecd2f42840f6d1f59855ba154629154
Lo Leon <lonelyliar8523@gmail.com>
2023-10-12 15:55:00 +0800

    rust 70: added solution of dp

--- rust/70.climbing-stairs.rs
+++ rust/70.climbing-stairs.rs
@@ -7,20 +7,27 @@
 // @lc code=start
 impl Solution {
     pub fn climb_stairs(n: i32) -> i32 {
-        use std::collections::HashMap;
-        let mut memo = HashMap::from([(1, 1), (2, 2)]);
-        fn climb(n: i32, memo: &mut HashMap<i32, i32>) -> i32 {
-            match memo.get(&n) {
-                Some(&r) => return r,
-                None => {
-                    let r = climb(n - 1, memo) + climb(n - 2, memo);
-                    memo.insert(n, r);
-                    return r;
-                }
-            }
-        }
+        // Solution 1:
+        // use std::collections::HashMap;
+        // let mut memo = HashMap::from([(1, 1), (2, 2)]);
+        // fn climb(n: i32, memo: &mut HashMap<i32, i32>) -> i32 {
+        //     match memo.get(&n) {
+        //         Some(&r) => return r,
+        //         None => {
+        //             let r = climb(n - 1, memo) + climb(n - 2, memo);
+        //             memo.insert(n, r);
+        //             return r;
+        //         }
+        //     }
+        // }
+        // return climb(n, &mut memo);
 
-        return climb(n, &mut memo);
+        // Solution 2:
+        let mut dp = vec![1, 2];
+        for i in 2..(n as usize) {
+            dp.push(dp[i - 2] + dp[i - 1]);
+        }
+        return dp[n as usize - 1];
     }
 }
 // @lc code=end
