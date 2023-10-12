2a05331ce4dcda9886369e1dbed916e6e360bce5
Lo Leon <lonelyliar8523@gmail.com>
2023-10-12 16:08:26 +0800

    rust 26: added solution of two pointers

--- rust/26.remove-duplicates-from-sorted-array.rs
+++ rust/26.remove-duplicates-from-sorted-array.rs
@@ -7,8 +7,20 @@
 // @lc code=start
 impl Solution {
     pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-        nums.dedup();
-        return nums.len() as i32;
+        // Solution 1:
+        // nums.dedup();
+        // return nums.len() as i32;
+
+        // Solution 2: two pointers
+        let (mut s, mut f): (usize, usize) = (0, 1);
+        while f < nums.len() {
+            if nums[s] != nums[f] {
+                s += 1;
+                nums.swap(s, f);
+            }
+            f += 1;
+        }
+        return (s + 1) as i32;
     }
 }
 // @lc code=end
