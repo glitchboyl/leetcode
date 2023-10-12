d6e15b0024a90e8c96201ee81c8de5b0030190a4
Lo Leon <lonelyliar8523@gmail.com>
2023-10-12 17:56:20 +0800

    javascript 2

---
+++ javascript/2.add-two-numbers.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=2 lang=javascript
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let carry = 0;
+  const result = new ListNode();
+  let p = result;
+  while (l1 != null || l2 != null || carry != 0) {
+    let val = carry;
+    if (l1) {
+      val += l1.val;
+      l1 = l1.next;
+    }
+    if (l2) {
+      val += l2.val;
+      l2 = l2.next;
+    }
+    carry = ~~(val / 10);
+    p.next = new ListNode(val % 10);
+    p = p.next;
+  }
+  return result.next;
+};
+// @lc code=end
