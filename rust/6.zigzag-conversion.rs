a83d3176e7e430504573fec4692c7c8316ea58bd
Lo Leon <lonelyliar8523@gmail.com>
2023-10-13 10:58:51 +0800

    rust 6

---
+++ rust/6.zigzag-conversion.rs
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=6 lang=rust
+ *
+ * [6] Zigzag Conversion
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn convert(s: String, num_rows: i32) -> String {
+        if num_rows == 1 {
+            return s;
+        }
+        let mut zigzag = String::new();
+        let bytes = s.as_bytes();
+        for row in 0..num_rows {
+            let mut i = row as usize;
+            let mut d = true;
+            while i < s.len() {
+                zigzag.push(bytes[i].into());
+                let mut n = 0;
+                if row == 0 || row == num_rows - 1 {
+                    n = num_rows - 1;
+                } else if d {
+                    n = num_rows - row - 1;
+                } else {
+                    n = row;
+                }
+                i += (2 * n).max(1) as usize;
+                d = !d;
+            }
+        }
+        return zigzag;
+    }
+}
+// @lc code=end
