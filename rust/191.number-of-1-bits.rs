f13c38a480d8960f40fbcce7bfc7b83e7b31e133
Lo Leon <lonelyliar8523@gmail.com>
2023-10-16 22:15:24 +0800

    rust 191

--- rust/191.number-of-1-bits.rs
+++ rust/191.number-of-1-bits.rs
@@ -7,12 +7,16 @@
 // @lc code=start
 impl Solution {
     pub fn hammingWeight(n: u32) -> i32 {
-        let (mut result, mut x) = (0, n);
-        for i in 0..32 {
-            result += (x & 1) as i32;
-            x >>= 1;
-        }
-        return result;
+        // Solution 1:
+        return n.count_ones() as i32;
+
+        // Solution 2:
+        // let (mut result, mut x) = (0, n);
+        // for i in 0..32 {
+        //     result += (x & 1) as i32;
+        //     x >>= 1;
+        // }
+        // return result;
     }
 }
 // @lc code=end
