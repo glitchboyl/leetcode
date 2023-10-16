9af4e57829afb2faaed76ba514fc17d1e464fa3e
Lo Leon <lonelyliar8523@gmail.com>
2023-10-16 23:40:45 +0800

    rust 389

--- rust/389.find-the-difference.rs
+++ rust/389.find-the-difference.rs
@@ -7,14 +7,25 @@
 // @lc code=start
 impl Solution {
     pub fn find_the_difference(s: String, t: String) -> char {
-        let mut chars = vec![0; 26];
+        // Solution 1:
+        // let mut chars = vec![0; 26];
+        // for c in t.bytes() {
+        //     chars[(c - b'a') as usize] += 1;
+        // }
+        // for c in s.bytes() {
+        //     chars[(c - b'a') as usize] -= 1;
+        // }
+        // return (chars.iter().position(|c| *c == 1).unwrap() as u8 + b'a') as char;
+
+        // Solution 2: using XOR
+        let mut diff = 0;
         for c in t.bytes() {
-            chars[(c - b'a') as usize] += 1;
+            diff ^= c;
         }
         for c in s.bytes() {
-            chars[(c - b'a') as usize] -= 1;
+            diff ^= c;
         }
-        return (chars.iter().position(|c| *c == 1).unwrap() as u8 + b'a') as char;
+        return diff as char;
     }
 }
 // @lc code=end
