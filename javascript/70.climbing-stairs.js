6f792980cea3fd5bbd796f9a7fcae790642fea43
Lo Leon <lonelyliar8523@gmail.com>
2023-10-16 21:17:13 +0800

    javascript 70

--- javascript/70.climbing-stairs.js
+++ javascript/70.climbing-stairs.js
@@ -21,10 +21,16 @@ var climbStairs = function (n) {
   // return climb(n);
 
   // Solution 2: dp
-  const dp = [1, 2];
-  for (let i = 2; i < n; i++) {
-    dp.push(dp[i - 2] + dp[i - 1]);
-  }
-  return dp[n - 1];
+  // const dp = [1, 2];
+  // for (let i = 2; i < n; i++) {
+  //   dp.push(dp[i - 2] + dp[i - 1]);
+  // }
+  // return dp[n - 1];
+
+  // Solution 3:
+  let p = 0,
+    q = 1;
+  for (let i = 1; i < n; i++) [p, q] = [q, p + q];
+  return p + q;
 };
 // @lc code=end
