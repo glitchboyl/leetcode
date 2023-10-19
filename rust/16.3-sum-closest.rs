dfb557dc747f15ce162ba740dd0d02de318e86e0
Lo Leon <lonelyliar8523@gmail.com>
2023-10-19 16:07:25 +0800

    rust 16

---
+++ rust/16.3-sum-closest.rs
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=16 lang=rust
+ *
+ * [16] 3Sum Closest
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn three_sum_closest(nums: Vec<i32>, target: i32) -> i32 {
+        let mut ordered_nums = nums;
+        ordered_nums.sort();
+        let mut d = i32::MAX;
+        let len = ordered_nums.len();
+        for i in 0..len - 2 {
+            let (mut j, mut k) = (i + 1, len - 1);
+            while j < k {
+                let _d = target - ordered_nums[i] - ordered_nums[j] - ordered_nums[k];
+                if _d == 0 {
+                    return target;
+                } else if _d > 0 {
+                    j += 1;
+                } else {
+                    k -= 1;
+                }
+                if _d.abs() < d.abs() {
+                    d = _d;
+                }
+            }
+        }
+        return target - d;
+    }
+}
+// @lc code=end
