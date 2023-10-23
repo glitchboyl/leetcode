6b5585e07f051696765465a46b4970a35829f622
Lo Leon <lonelyliar8523@gmail.com>
2023-10-23 15:51:23 +0800

    rust 22

---
+++ rust/22.generate-parentheses.rs
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=22 lang=rust
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        let mut parentheses: Vec<String> = vec![];
+        fn backtrack_insert(
+            l: i32,
+            r: i32,
+            n: i32,
+            parenthesis: String,
+            parentheses: &mut Vec<String>,
+        ) {
+            if l == r && r == n {
+                parentheses.push(parenthesis);
+                return;
+            }
+            if l < n {
+                backtrack_insert(l + 1, r, n, format!("{parenthesis}("), parentheses);
+            }
+            if r < l && r < n {
+                backtrack_insert(l, r + 1, n, format!("{parenthesis})"), parentheses);
+            }
+        }
+        backtrack_insert(0, 0, n, String::new(), &mut parentheses);
+        parentheses
+    }
+}
+// @lc code=end
