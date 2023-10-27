2eb011225a531ebf1848df3bdc23173fb88c9772
Lo Leon <lonelyliar8523@gmail.com>
2023-10-27 10:43:57 +0800

    rust 455

--- rust/455.assign-cookies.rs
+++ rust/455.assign-cookies.rs
@@ -7,22 +7,20 @@
 // @lc code=start
 impl Solution {
     pub fn find_content_children(g: Vec<i32>, s: Vec<i32>) -> i32 {
-        if s.len() == 0 {
-            return 0;
-        }
-
-        let mut g = g;
-        let mut s = s;
-        g.sort_unstable();
-        s.sort_unstable();
         let mut content = 0;
-        let (mut i, mut j): (usize, usize) = (0, 0);
-        while i < g.len() && j < s.len() {
-            if s[j] >= g[i] {
-                content += 1;
-                i += 1; 
+        if s.len() > 0 {
+            let mut g = g;
+            let mut s = s;
+            g.sort_unstable();
+            s.sort_unstable();
+            let (mut i, mut j): (usize, usize) = (0, 0);
+            while i < g.len() && j < s.len() {
+                if s[j] >= g[i] {
+                    content += 1;
+                    i += 1;
+                }
+                j += 1;
             }
-            j += 1;
         }
         content
     }
