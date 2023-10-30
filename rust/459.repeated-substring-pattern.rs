9d19f7f6fc09ab495a8e2f5c59c21cedebcbb451
Lo Leon <lonelyliar8523@gmail.com>
2023-10-30 17:28:37 +0800

    rust 459

--- rust/459.repeated-substring-pattern.rs
+++ rust/459.repeated-substring-pattern.rs
@@ -8,12 +8,17 @@
 impl Solution {
     pub fn repeated_substring_pattern(s: String) -> bool {
         let len = s.len();
-        for i in 1..=len / 2 {
-            if len % i == 0 && s[..i].repeat(len / i) == s {
-                return true;
-            }
-        }
-        false
+
+        // Solution 1:
+        // for i in 1..=len / 2 {
+        //     if len % i == 0 && s[..i].repeat(len / i) == s {
+        //         return true;
+        //     }
+        // }
+        // false
+
+        // Solution 2:
+        s.repeat(2)[1..len * 2 - 1].contains(&s)
     }
 }
 // @lc code=end
