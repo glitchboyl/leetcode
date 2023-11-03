f27283effdd705039a3642750dec7aa1c1b8507d
Lo Leon <lonelyliar8523@gmail.com>
2023-11-03 09:14:18 +0800

    rust 40

---
+++ rust/40.combination-sum-ii.rs
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=40 lang=rust
+ *
+ * [40] Combination Sum II
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn combination_sum2(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+        let mut combinations = vec![];
+        let mut candidates = candidates;
+        candidates.sort();
+        fn backtrack(
+            candidates: &Vec<i32>,
+            target: i32,
+            s: usize,
+            combination: &mut Vec<i32>,
+            combinations: &mut Vec<Vec<i32>>,
+        ) {
+            for i in s..candidates.len() {
+                if i > s && candidates[i] == candidates[i - 1] {
+                    continue;
+                }
+                if candidates[i] > target {
+                    return;
+                }
+                combination.push(candidates[i]);
+                if candidates[i] == target {
+                    combinations.push(combination.clone());
+                    combination.pop();
+                    break;
+                }
+                backtrack(
+                    candidates,
+                    target - candidates[i],
+                    i + 1,
+                    combination,
+                    combinations,
+                );
+                combination.pop();
+            }
+        }
+        backtrack(&candidates, target, 0, &mut vec![], &mut combinations);
+        combinations
+    }
+}
+// @lc code=end
