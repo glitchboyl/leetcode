8b0fb47369811acb245f09df52753a5a6abf2754
Lo Leon <lonelyliar8523@gmail.com>
2023-11-09 22:42:49 +0800

    rust 49

---
+++ rust/49.group-anagrams.rs
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=49 lang=rust
+ *
+ * [49] Group Anagrams
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        use std::collections::HashMap;
+        let mut anagrams = HashMap::new();
+
+        for word in strs {
+            let mut key: Vec<_> = word.chars().collect();
+            key.sort();
+            anagrams.entry(key).or_insert(vec![]).push(word);
+        }
+
+        anagrams.into_values().collect::<Vec<_>>()
+    }
+}
+// @lc code=end
