f0104110beda83232e8c97cadb8b3cdcb0492e1c
Lo Leon <lonelyliar8523@gmail.com>
2023-11-18 18:13:36 +0800

    rust 57

--- rust/57.insert-interval.rs
+++ rust/57.insert-interval.rs
@@ -7,15 +7,12 @@
 // @lc code=start
 impl Solution {
     pub fn insert(intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
-        if intervals.len() == 0 {
-            return vec![new_interval.to_vec()];
-        }
         let mut intervals = intervals;
-        let mut new_interval = new_interval;
-        if intervals[intervals.len() - 1][1] < new_interval[0] {
+        if intervals.len() == 0 || intervals[intervals.len() - 1][1] < new_interval[0] {
             intervals.push(new_interval);
             return intervals;
         }
+        let mut new_interval = new_interval;
         let l = intervals.partition_point(|interval| interval[1] < new_interval[0]);
         let r = intervals[l..].partition_point(|interval| interval[0] <= new_interval[1]) + l;
         let slice: Vec<Vec<i32>> = intervals.drain(l..r).collect();
