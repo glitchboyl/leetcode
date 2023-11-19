cc62312e7ce99cbcb70d3139186d808ef19d734f
Lo Leon <lonelyliar8523@gmail.com>
2023-11-19 14:21:07 +0800

    rust 59

--- rust/59.spiral-matrix-ii.rs
+++ rust/59.spiral-matrix-ii.rs
@@ -11,34 +11,25 @@ impl Solution {
         let mut matrix = vec![vec![0; n]; n];
         let (mut top, mut right, mut bottom, mut left) = (0, n, n, 0);
         let mut k = 1;
-        loop {
-            if left == right {
-                break;
-            }
+        while k <= (n as i32).pow(2) {
             for j in left..right {
                 matrix[top][j] = k;
                 k += 1;
             }
             top += 1;
-            if top == bottom {
-                break;
-            }
+
             for i in top..bottom {
                 matrix[i][right - 1] = k;
                 k += 1;
             }
             right -= 1;
-            if left == right {
-                break;
-            }
+
             for j in (left..right).rev() {
                 matrix[bottom - 1][j] = k;
                 k += 1;
             }
             bottom -= 1;
-            if top == bottom {
-                break;
-            }
+
             for i in (top..bottom).rev() {
                 matrix[i][left] = k;
                 k += 1;
