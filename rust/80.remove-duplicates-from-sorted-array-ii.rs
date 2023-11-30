d3ce519ee8c49d03719d5d92cc9e42427e7ec5db
Lo Leon <lonelyliar8523@gmail.com>
2023-11-30 16:54:50 +0800

    rust 80

---
+++ rust/80.remove-duplicates-from-sorted-array-ii.rs
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=80 lang=rust
+ *
+ * [80] Remove Duplicates from Sorted Array II
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut current = nums[0];
+        let mut count = 1;
+        let mut s = 1;
+        for f in 1..nums.len() {
+            if nums[f] != current {
+                current = nums[f];
+                nums.swap(s, f);
+                count = 1;
+                s += 1;
+            } else if count < 2 {
+                nums.swap(s, f);
+                count += 1;
+                s += 1;
+            }
+        }
+        s as i32
+    }
+}
+// @lc code=end
