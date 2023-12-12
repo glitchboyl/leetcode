8cce1400efec02ab98b2d07c167f06a42f58ee39
Lo Leon <lonelyliar8523@gmail.com>
2023-12-12 19:10:47 +0800

    rust 645

--- rust/645.set-mismatch.rs
+++ rust/645.set-mismatch.rs
@@ -7,20 +7,16 @@
 // @lc code=start
 impl Solution {
     pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
-        use std::collections::HashSet;
-        let mut set = HashSet::with_capacity(nums.len());
         let n = nums.len() as i32;
-        let (mut duplicate, mut missing) = (0, 0);
+        let mut arr = vec![0; nums.len()];
+        let (mut duplicate, mut missing) = (0, n * (n + 1) / 2);
         for num in nums {
-            if !set.insert(num) {
+            if arr[num as usize - 1] == 1 {
                 duplicate = num;
+                continue;
             }
-        }
-        for i in 1..=n {
-            if !set.contains(&i) {
-                missing = i;
-                break;
-            }
+            arr[num as usize - 1] = 1;
+            missing -= num;
         }
         vec![duplicate, missing]
     }
