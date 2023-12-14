d611f57d29956fb4928a648ac00a018782fd8f7a
Lo Leon <lonelyliar8523@gmail.com>
2023-12-14 19:56:25 +0800

    javascript 661

---
+++ javascript/661.image-smoother.js
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=661 lang=javascript
+ *
+ * [661] Image Smoother
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} img
+ * @return {number[][]}
+ */
+var imageSmoother = function (img) {
+  const m = img.length,
+    n = img[0].length;
+  return img.map((row, i) =>
+    row.map((cell, j) => {
+      let c = 1;
+      if (i > 0) {
+        const prevRow = img[i - 1];
+        cell += prevRow[j];
+        c++;
+        if (j > 0) {
+          cell += prevRow[j - 1];
+          c++;
+        }
+        if (j < n - 1) {
+          cell += prevRow[j + 1];
+          c++;
+        }
+      }
+      if (i < m - 1) {
+        const nextRow = img[i + 1];
+        cell += nextRow[j];
+        c++;
+        if (j > 0) {
+          cell += nextRow[j - 1];
+          c++;
+        }
+        if (j < n - 1) {
+          cell += nextRow[j + 1];
+          c++;
+        }
+      }
+      if (j > 0) {
+        cell += row[j - 1];
+        c++;
+      }
+      if (j < n - 1) {
+        cell += row[j + 1];
+        c++;
+      }
+
+      return ~~(cell / c);
+    })
+  );
+};
+// @lc code=end
